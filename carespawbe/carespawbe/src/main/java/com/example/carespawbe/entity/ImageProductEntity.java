cf944e369d3fc25ab818a345e082e99111232826
ngannguyenkim203 <ngannguyenkim0711@gmail.com>
2025-07-01 08:17:47 +0700

    DB product

---
+++ carespawbe/carespawbe/src/main/java/com/example/carespawbe/entity/ImageProductEntity.java
@@ -0,0 +1,35 @@
+package com.example.carespawbe.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+
+@Entity
+@Table(name = "images_product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ImageProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "imageProduct_id")
+    private Long imageProductId;
+
+    @Column(nullable = false)
+    private String imageProduct_url;
+
+//    @Column(nullable = false)
+//    private Long source_id;
+
+//    @Column(nullable = false)
+//    private String source_type;
+
+    @Column(nullable = false)
+    private Date uploaded_at;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", insertable = false, updatable = false)
+    private ProductEntity imageProduct;
+}
