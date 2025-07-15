f3fe66e4b4a3b480b9d5b5e18b6fbd0b0cda0624
wadashinichi <edogawa280723@gmail.com>
2025-07-16 05:42:23 +0700

    take be at 16/7

--- carespawbe/carespawbe/src/main/java/com/example/carespawbe/mapper/PostCommentMapper.java
+++ carespawbe/carespawbe/src/main/java/com/example/carespawbe/mapper/PostCommentMapper.java
@@ -1,14 +1,23 @@
 package com.example.carespawbe.mapper;
 
+import com.example.carespawbe.dto.Forum.PostCommentRequest;
 import com.example.carespawbe.dto.Forum.PostCommentResponse;
 import com.example.carespawbe.entity.PostComment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PostCommentMapper {
+    @Mapping(source = "user.avatar", target = "avatar")
+    @Mapping(source = "user.fullname", target = "fullname")
+    @Mapping(source = "user.id", target = "userId")
     PostCommentResponse toCommentResponse(PostComment postComment);
 
     List<PostCommentResponse> toCommentResponseList(List<PostComment> postComments);
+
+    @Mapping(source = "userId", target = "user.id")
+    @Mapping(source = "postId", target = "post.id")
+    PostComment toPostComment(PostCommentRequest postCommentRequest);
 }
