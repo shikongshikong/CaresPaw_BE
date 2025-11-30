c5d1ea074e7bc7d9bd5514ab57ecfff127b5b191
Nguyen Ngan <nguyenngan@Nguyens-MacBook-Air.local>
2025-11-30 16:34:07 +0700

    Remove duplicated carespawbe folder

--- carespawbe/carespawbe/src/main/java/com/example/carespawbe/dto/Forum/ForumPageFilterResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.carespawbe.dto.Forum;
-
-import lombok.Data;
-import org.springframework.data.domain.Page;
-
-@Data
-public class ForumPageFilterResponse {
-    Page<ShortForumPostResponse> postList;
-}
--- carespawbe/src/main/java/com/example/carespawbe/service/VarriantService.java
+++
@@ -1,9 +0,0 @@
-package com.example.carespawbe.service;
-
-import com.example.carespawbe.entity.shop.VarriantEntity;
-
-import java.util.List;
-
-public interface VarriantService {
-    List<VarriantEntity> getAllVarriants();
-}
